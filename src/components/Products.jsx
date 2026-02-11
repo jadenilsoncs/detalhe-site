f5f5b69955e6c70df1d1d09eff7b8ab35d3758a6
jadenilsoncs <jadenilsoncs@gmail.com>
2026-02-11 20:26:58 -0300

    feat: 'Link <-TODOS OS PRODUTO' somente nas categorias da Home.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -85,10 +85,12 @@ const Products = () => {
           )}
         </div>
       </div>
-      {/* Adição mínima: div para centralizar */}
-      <div style={{ textAlign: 'center', margin: '20px 0' }}>  {/* margin opcional para espaçamento */}
-        <a className="btn-voltar-discreto" href="/detalhe/products">← Todos os produtos</a>
-      </div>
+      {/* Link "← Todos os produtos" só aparece em categorias (NÃO na página principal) */}
+      {category && (
+        <div style={{ textAlign: 'center', margin: '20px 0' }}>
+          <a className="btn-voltar-discreto" href="/detalhe/products">← Todos os produtos</a>
+        </div>
+      )}
     </div>
   );
 };
